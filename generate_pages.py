168fe470bf007e94749c0cc07bf5772cc1674428
Edel Grace <edel.altares@gmail.com>
2017-07-09 15:09:50 -0600

    Update generate_pages.py - comments

--- generate_pages.py
+++ generate_pages.py
@@ -13,18 +13,25 @@ def run_program():
         program using the arguments or the default values
     """
 
+    # create argument parser instance
     arg_parser = argparse.ArgumentParser(
         description='Use your own TMdb list ID to generate a web page.')
 
+    # add an argument for the list ID
     arg_parser.add_argument(
-        '--list-id', metavar='list_id', nargs='?',
+        '--list-id', metavar='list_id', nargs='?',  # optional argument  
         help='the list ID of the TMdb list')
 
+    # go through and parse the argument
     arguments = arg_parser.parse_args()
 
-    arguments = arguments.list_id
+    # get the list_id argument
+    list_id = arguments.list_id
 
-    get_movie_list(arguments)
+    # get the movie list with the list id
+    get_movie_list(list_id)
+
+    return
 
 
 def get_movie_list(args):
@@ -37,12 +44,12 @@ def get_movie_list(args):
     movies = []
 
     # check if a list ID was specified at the command line
-    if args is None:
-        list_id = "27917"           # use the default list
-    else:
-        list_id = args   # use the list specified
+    if args is None:    # no list ID: use default list
+        list_id = "27917"           
+    else:               # list ID: use specified ID
+        list_id = args   
 
-    # using themoviedb.org API
+    # form the API request to get the list using the list ID
     get_list = "https://api.themoviedb.org/4/list/"
     get_list += list_id
     get_list += "?api_key=f99429863a7d560f97d2997d4b602460"
@@ -55,21 +62,21 @@ def get_movie_list(args):
         # the list ID specified was not valid
         print("The list ID " + list_id + " is not valid")
 
-        # program exits
+        # exit the program
         return
 
     # convert the response to json format
-    movie_list = request.json()
+    tmdb_list = request.json()
 
     # get the list details from the response
-    description = movie_list['description']
-    list_title = movie_list['name']
-    list_author = movie_list['created_by']['username']
+    description = tmdb_list['description']
+    list_title = tmdb_list['name']
+    list_author = tmdb_list['created_by']['username']
 
     # process the list and put them into an array
-    movies = process_movie_list(movie_list)
+    movies = process_movie_list(tmdb_list)
 
-    #
+    # use the movie list and list details in creating the HTML pages
     fresh_tomatoes.open_movies_page(movies, list_title,
                                     list_author, description)
 
@@ -127,22 +134,31 @@ def get_trailer(movie_id):
     """ This function grabs the YouTube trailer URL from the movie using
         a specific call to the API
     """
+    
+    # initialize trailer URL variable
+    trailer = ""
 
     # structure the URL for the API request
     url = "https://api.themoviedb.org/3/movie/"
     url += str(movie_id)  #
     url += "/videos?api_key=f99429863a7d560f97d2997d4b602460"
 
-    trailer = requests.get(url)
+    # send the request
+    request = requests.get(url)
 
-    trailer = trailer.json()
+    # convert the response to json format
+    videos = request.json()
 
-    for video in trailer['results']:
-        if video['site'] == "YouTube":
+    # go through each video associated with the movie to find YouTube URL
+    for video in videos['results']:
+        # check if the video is a YouTube video and if it is a trailer
+        if video['site'] == "YouTube" and video['type'] == "Trailer":
             trailer = "http://youtube.com/watch?v=" + video['key']
             break
-
+    
+    # check if no trailer is found
     if "http://" not in trailer:
+        # use a default placeholder YouTube video for the trailer
         trailer = "https://www.youtube.com/watch?v=D-CQVnuiR1I"
 
     return trailer
