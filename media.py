a5e08be68871bbc7dbc15ecad2ea364b23e03464
Edel Grace <edel.altares@gmail.com>
2017-07-09 14:43:07 -0600

    Update media.py - comments

--- media.py
+++ media.py
@@ -4,11 +4,24 @@ import webbrowser
 
 
 class Movie():
-    """ This class stores movie information """
+    """ This class stores information about movies
+        
+        Attributes:
+                title (str): The movie title
+                storyline (str): A brief overview of the movie storyline
+                poster (str): URL to the path of movie poster image
+                trailer (str): YouTube URL to the trailer video
+    """
 
     def __init__(self, title, storyline, poster, trailer):
-        """ The constructor will initialize the variables using the
-            arguments provided
+        """ The constructor will initialize the attributes of a movie instance
+            using the given arguments
+            
+            Args:
+                title (str): The movie title
+                storyline (str): A brief overview of the movie storyline
+                poster (str): URL to the path of movie poster image
+                trailer (str): YouTube URL to the trailer video
         """
 
         self.title = title
